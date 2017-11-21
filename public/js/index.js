4ba2ee33286488545ac9d7ceaaa82f0b3b3ea3c2
jianshentan <jianshentan@gmail.com>
2017-11-21 00:31:33 +0000

    video

--- public/js/index.js
+++ public/js/index.js
@@ -148,3 +148,25 @@ function isValidEmailAddress(emailAddress) {
   var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
   return pattern.test(emailAddress);
 };
+
+/* ======= MODAL ======= */
+   
+var jf_modal = document.getElementById("jf-modal");
+
+// When the user clicks the button, open the modal 
+document.getElementById("jf-video-trigger").onclick = function() { 
+  jf_modal.style.display = "block"; 
+};
+// Get the <span> element that closes the modal // When the user clicks on <span> (x), close the modal
+document.getElementById("jf-close").onclick = function() { 
+  jf_modal.style.display = "none"; 
+  my_video_player.pause();
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == jf_modal) {
+    jf_modal.style.display = "none";
+    my_video_player.pause();
+  }
+};
